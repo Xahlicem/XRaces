c008a9d213d82a1eb674b36f4872cdb2fec719d2
Xahlicem <xahlicem@gmail.com>
2017-08-19 15:59:27 -0700

    Added recipe group.

--- XRaces.cs
+++ XRaces.cs
@@ -1,17 +1,30 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace XRaces
-{
-	class XRaces : Mod
-	{
-		public XRaces()
-		{
-			Properties = new ModProperties()
-			{
-				Autoload = true,
-				AutoloadGores = true,
-				AutoloadSounds = true
-			};
-		}
-	}
+namespace XRaces {
+    class XRaces : Mod {
+        public XRaces() {
+            Properties = new ModProperties() {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+
+        public override void AddRecipeGroups() {
+            RecipeGroup group = new RecipeGroup(() => Lang.misc[37] + " Soul", new int[] {
+                this.ItemType<Items.Misc.AntSoul>(),
+                this.ItemType<Items.Misc.DemonSoul>(),
+                this.ItemType<Items.Misc.GoblinSoul>(),
+                this.ItemType<Items.Misc.LizardmanSoul>(),
+                this.ItemType<Items.Misc.RobotSoul>(),
+                this.ItemType<Items.Misc.ShadeSoul>(),
+                this.ItemType<Items.Misc.SkeletonSoul>(),
+                this.ItemType<Items.Misc.SlimeSoul>(),
+                this.ItemType<Items.Misc.ZombieSoul>()
+            });
+            RecipeGroup.RegisterGroup("XRSouls", group);
+        }
+    }
 }
